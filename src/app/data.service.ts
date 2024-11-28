d8b0ea6033f55a22fa1437a2f55585763c9cc762
Chris DeForest <ced6563@rit.edu>
2024-11-28 18:00:56 -0500

    allowed actual display of todos and todos completed state instead of hardcoded html

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -6,7 +6,26 @@ import { Todo } from './todo';
 })
 export class DataService {
 
-  todos: Todo[] = [];
-  
+  todos: Todo[] = [
+    {text: "FINISH TESTING TODO APP", completed: true}, 
+    {text:  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas incidunt expedita dicta nobis autem neque sed. Vero, sequi quos! Nulla exercitationem consectetur praesentium. Quis libero magni fuga consequatur, ipsum cum.", completed: false}
+  ];
+
   constructor() { }
+
+  getAllTodos() {
+    return this.todos;
+  }
+
+  addTodo(todo: Todo) {
+    this.todos.push(todo);
+  }
+
+  updateTodo(index: number, updatedTodo: Todo) {
+    this.todos[index] = updatedTodo;
+  }
+
+  deleteTodo(index: number) {
+    this.todos.splice(index, 1);
+  }
 }
--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { Todo } from '../todo';
+import { DataService } from '../data.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-todos',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule , FormsModule ],
   templateUrl: './todos.component.html',
   styleUrl: './todos.component.scss'
 })
-export class TodosComponent {
+export class TodosComponent implements OnInit{
+  todos: Todo[] = [];
+  
+  constructor(private readonly dataService: DataService) {
+  }
+  
+  ngOnInit(): void {
+    this.todos = this.dataService.getAllTodos();
+  }
 
+  getText(todo: Todo) {
+    return todo.text;
+  }
+
+  getCompleted(todo: Todo) {
+    return todo.completed;
+  }
+
+  onFormSubmit(form: NgForm): void {
+    console.log(form);
+  }
 }
